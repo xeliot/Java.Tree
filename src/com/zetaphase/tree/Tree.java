5a4f80b3137aa192a19a149db963b79297166660
Dave Ho <daveho8888@gmail.com>
2016-07-13 10:50:34 -0700

    implemented tree method - find(int id)

--- src/com/zetaphase/tree/Tree.java
+++ src/com/zetaphase/tree/Tree.java
@@ -1,7 +1,27 @@
 package com.zetaphase.tree;
 
 public class Tree {
-
+	
+	public Node root;
+	
+	public Tree(){
+		this.root = null;
+	}
+	
+	public boolean find(int id){
+		Node current = root;
+		while(current != null){
+			if(current.getData() == id){
+				return true;
+			}else if(current.getData() == id){
+				current = current.getLeft();
+			}else{
+				current = current.getRight();
+			}
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
